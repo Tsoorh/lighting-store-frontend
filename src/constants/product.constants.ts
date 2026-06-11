a900572b77609164ac1ba6b6a9ebed167252a1d8
Tsoorh <tsoor.hartov@gmail.com>
2026-06-11 18:38:17 +0300

    fix: remove duplicate wood type entry for American walnut in PRODUCT_OPTIONS

--- src/constants/product.constants.ts
+++ src/constants/product.constants.ts
@@ -1,7 +1,6 @@
 export const PRODUCT_OPTIONS = {
     WOOD_TYPES: [
         { en: 'Oak', he: 'אלון' },
-        { en: 'American walnut', he: 'אגוז אמריקאי' },
         { en: 'Oak stained as walnut', he: 'אלון מגוון לאגוז' },
         { en: 'No wood', he: 'ללא עץ' },
         { en: 'Oak/American walnut', he:'אלון/אגוז אמריקאי' }
