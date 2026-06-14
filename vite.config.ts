72bd2eb3f272193ce5bd99f3c2e3fd9d638d5c9b
Tsoorh <tsoor.hartov@gmail.com>
2026-06-14 17:59:42 +0300

    feat: update build output directory and add wrangler configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build:{
-    outDir: '../lighting-store-backend/public',
+    outDir: 'dist',
     emptyOutDir: true
   }
 })
